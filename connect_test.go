6b250619493b94d38da6ff327562ac246157b935
Vivek R <vividvilla@gmail.com>
2018-03-30 03:36:02 +0530

    Boiler plate to implement setup/teardown for API method tests

--- connect_test.go
+++ connect_test.go
@@ -2,10 +2,13 @@ package kiteconnect
 
 import (
 	"net/http"
+	"reflect"
+	"strings"
 	"testing"
 	"time"
 )
 
+// Test New Kite Connect instance
 func TestNewClient(t *testing.T) {
 	apiKey := "api_key"
 	client := New(apiKey)
@@ -15,6 +18,7 @@ func TestNewClient(t *testing.T) {
 	}
 }
 
+// Test all client setters
 func TestClientSetters(t *testing.T) {
 	apiKey := "kitefront"
 	client := New(apiKey)
@@ -68,4 +72,57 @@ func TestClientSetters(t *testing.T) {
 	if client.httpClient.GetClient().Timeout != customTimeout {
 		t.Errorf("HTTPClient timeout is not set properly.")
 	}
+}
+
+// Following boiler plate is used to implement setup/teardown using Go subtests feature
+
+// Test only function prefix with this
+const suiteTestMethodPrefix = "Test"
+
+// TestSuite is an interface where you define suite and test case preparation and tear down logic.
+type TestSuite struct{}
+
+// Setup the API suit
+func SetupAPITestSuit() {}
+
+// TearDown API suit
+func TearDownAPITestSuit() {}
+
+// Individual test setup
+func SetupAPITest() {}
+
+// Individual test teardown
+func TearDownAPITest() {}
+
+/*
+Run sets up the suite, runs its test cases and tears it down:
+    1. Calls `suite.SetUpSuite`
+    2. Seeks for any methods that have `Test` prefix, for each of them it:
+      a. Calls `SetUp`
+      b. Calls the test method itself
+      c. Calls `TearDown`
+    3. Calls `suite.TearDownSuite`
+*/
+func RunAPITests(t *testing.T, suite *TestSuite) {
+	SetupAPITestSuit()
+	defer TearDownAPITestSuit()
+
+	suiteType := reflect.TypeOf(suite)
+	for i := 0; i < suiteType.NumMethod(); i++ {
+		m := suiteType.Method(i)
+		if strings.HasPrefix(m.Name, suiteTestMethodPrefix) {
+			t.Run(m.Name, func(t *testing.T) {
+				SetupAPITest()
+				defer TearDownAPITest()
+
+				in := []reflect.Value{reflect.ValueOf(suite), reflect.ValueOf(t)}
+				m.Func.Call(in)
+			})
+		}
+	}
+}
+
+func TestAPIMethods(t *testing.T) {
+	s := &TestSuite{}
+	RunAPITests(t, s)
 }
