5d4a08f7339b9545df39996a250a90acb2b5e558
Rohan Verma <hello@rohanverma.net>
2021-06-30 12:25:11 +0530

    refactor: timestamp parsing logic
    Adds support for local tz parsing and combines parsing across both
    CSV and JSON parsers.

--- models/time.go
+++ models/time.go
@@ -11,24 +11,16 @@ type Time struct {
 }
 
 // List of known time formats
-var ctLayouts = []string{"2006-01-02", "2006-01-02 15:04:05", "2006-01-02T15:04:05-0700"}
+var (
+	ctLayouts      = []string{"2006-01-02", "2006-01-02 15:04:05"}
+	ctZonedLayouts = []string{"2006-01-02T15:04:05-0700"}
+)
 
 // UnmarshalJSON parses JSON time string with custom time formats
-func (t *Time) UnmarshalJSON(b []byte) (err error) {
-	var pTime time.Time
-	s := strings.TrimSpace(strings.Trim(string(b), "\""))
-
-	if s == "" || s == "null" {
-		t.Time = pTime
-		return nil
-	}
-
-	// Iterate through known layouts and parse time
-	for _, l := range ctLayouts {
-		pTime, err = time.Parse(l, s)
-		if err == nil && !pTime.IsZero() {
-			break
-		}
+func (t *Time) UnmarshalJSON(b []byte) error {
+	pTime, err := parseTime(string(b))
+	if err != nil {
+		return err
 	}
 
 	t.Time = pTime
@@ -36,16 +28,50 @@ func (t *Time) UnmarshalJSON(b []byte) (err error) {
 }
 
 // UnmarshalCSV converts CSV string field internal date
-func (t *Time) UnmarshalCSV(s string) (err error) {
-	var pTime time.Time
-	s = strings.TrimSpace(s)
+func (t *Time) UnmarshalCSV(s string) error {
+	pTime, err := parseTime(s)
+	if err != nil {
+		return err
+	}
+
+	t.Time = pTime
+	return nil
+}
+
+func parseTime(ts string) (time.Time, error) {
+	var (
+		pTime time.Time
+		err   error
+	)
+
+	s := strings.TrimSpace(strings.Trim(ts, "\""))
+	if s == "" || s == "null" {
+		return pTime, nil
+	}
+
+	// Load IST location.
+	loc, err := time.LoadLocation("Asia/Kolkata")
+	if err != nil {
+		return pTime, err
+	}
+
+	// Iterate through zoneless layouts and assign zone as IST.
 	for _, l := range ctLayouts {
-		pTime, err = time.Parse(l, s)
+		pTime, err = time.ParseInLocation(l, s, loc)
 		if err == nil && !pTime.IsZero() {
 			break
 		}
 	}
 
-	t.Time = pTime
-	return nil
+	// If pattern not found then iterate and parse layouts with zone.
+	if pTime.IsZero() {
+		for _, l := range ctZonedLayouts {
+			pTime, err = time.Parse(l, s)
+			if err == nil && !pTime.IsZero() {
+				break
+			}
+		}
+	}
+
+	return pTime, err
 }
