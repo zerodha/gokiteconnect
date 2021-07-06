708214f8e561f13dd1f8b900ae119273794967e7
Rakesh R <ranjan01995@gmail.com>
2021-07-06 14:43:57 +0530

    feat: add compact mode margin and basket order margins

--- margins.go
+++ margins.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-// OrderMarginParam represents a position in the Margin Calculator API
+// OrderMarginParam represents an order in the Margin Calculator API
 type OrderMarginParam struct {
 	Exchange        string  `json:"exchange"`
 	Tradingsymbol   string  `json:"tradingsymbol"`
@@ -41,6 +41,28 @@ type OrderMargins struct {
 	Total         float64 `json:"total"`
 }
 
+// OrderMarginsCompact represents compact mode response from the Margin Calculator API.
+type OrderMarginsCompact struct {
+	Type          string  `json:"type"`
+	TradingSymbol string  `json:"tradingsymbol"`
+	Exchange      string  `json:"exchange"`
+	Total         float64 `json:"total"`
+}
+
+// BaksetMargins represents response from the Margin Calculator API for Basket orders
+type BaksetMargins struct {
+	Initial OrderMargins   `json:"initial"`
+	Final   OrderMargins   `json:"final"`
+	Orders  []OrderMargins `json:"orders"`
+}
+
+// BaksetMarginsCompact represents compact mode response from the Margin Calculator API for Basket orders
+type BaksetMarginsCompact struct {
+	Initial OrderMarginsCompact   `json:"initial"`
+	Final   OrderMarginsCompact   `json:"final"`
+	Orders  []OrderMarginsCompact `json:"orders"`
+}
+
 func (c *Client) GetOrderMargins(orderParams []OrderMarginParam) ([]OrderMargins, error) {
 	body, err := json.Marshal(orderParams)
 	if err != nil {
@@ -58,6 +80,84 @@ func (c *Client) GetOrderMargins(orderParams []OrderMarginParam) ([]OrderMargins
 	var out []OrderMargins
 	if err := readEnvelope(resp, &out); err != nil {
 		return []OrderMargins{}, err
+	}
+
+	return out, nil
+}
+
+func (c *Client) GetCompactOrderMargins(orderParams []OrderMarginParam) ([]OrderMarginsCompact, error) {
+	body, err := json.Marshal(orderParams)
+	if err != nil {
+		return []OrderMarginsCompact{}, err
+	}
+
+	var headers http.Header = map[string][]string{}
+	headers.Add("Content-Type", "application/json")
+
+	uri := URIOrderMargins + "?mode=compact"
+
+	resp, err := c.doRaw(http.MethodPost, uri, body, headers)
+	if err != nil {
+		return []OrderMarginsCompact{}, err
+	}
+
+	var out []OrderMarginsCompact
+	if err := readEnvelope(resp, &out); err != nil {
+		return []OrderMarginsCompact{}, err
+	}
+
+	return out, nil
+}
+
+func (c *Client) GetBasketMargins(orderParams []OrderMarginParam, considerPositions bool) (BaksetMargins, error) {
+	body, err := json.Marshal(orderParams)
+	if err != nil {
+		return BaksetMargins{}, err
+	}
+
+	var headers http.Header = map[string][]string{}
+	headers.Add("Content-Type", "application/json")
+
+	uri := URIBasketMargins
+	if considerPositions {
+		uri += "?consider_positions=true"
+	}
+
+	resp, err := c.doRaw(http.MethodPost, uri, body, headers)
+	if err != nil {
+		return BaksetMargins{}, err
+	}
+
+	var out BaksetMargins
+	if err := readEnvelope(resp, &out); err != nil {
+		return BaksetMargins{}, err
+	}
+
+	return out, nil
+}
+
+func (c *Client) GetBasketCompactMargins(orderParams []OrderMarginParam, considerPositions bool) (BaksetMarginsCompact, error) {
+	body, err := json.Marshal(orderParams)
+	if err != nil {
+		return BaksetMarginsCompact{}, err
+	}
+
+	var headers http.Header = map[string][]string{}
+	headers.Add("Content-Type", "application/json")
+
+	uri := URIBasketMargins + "?mode=compact"
+	if considerPositions {
+		uri += "&consider_positions=true"
+	}
+
+	resp, err := c.doRaw(http.MethodPost, uri, body, headers)
+	if err != nil {
+		return BaksetMarginsCompact{}, err
+	}
+
+	var out BaksetMarginsCompact
+	if err := readEnvelope(resp, &out); err != nil {
+		return BaksetMarginsCompact{}, err
 	}
 
 	return out, nil
